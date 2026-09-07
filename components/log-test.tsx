2add3030b6f36dcdc118d008542d46f70425578c
thetalemon <sasami0314@gmail.com>
2026-09-07 17:55:17 +0900

    ログ追加

--- components/log-test.tsx
+++ components/log-test.tsx
@@ -4,7 +4,15 @@ import { useEffect } from 'react'
 export default function LogTest() {
   useEffect(() => {
     console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
-    console.log('KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+    console.log('ANON KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+    console.log(
+      'PUBLISHABLE KEY (exists):',
+      !!process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+    )
+    console.log(
+      'PUBLISHABLE KEY (value):',
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+    )
   }, [])
   return null
 }
