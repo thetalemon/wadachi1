efc4337808fab061d191646a0d14785bc10e1251
thetalemon <sasami0314@gmail.com>
2026-09-07 18:08:54 +0900

    cacheComponentsを基に戻す

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {/* cacheComponents: true, */}
+const nextConfig: NextConfig = { cacheComponents: true }
 
 export default nextConfig
