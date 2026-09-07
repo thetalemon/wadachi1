bdf0d85019e9af9671b7dfc024c98da8dfd0d058
thetalemon <sasami0314@gmail.com>
2026-09-07 17:22:14 +0900

    ログ追加

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,15 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
+  console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log(
+    'KEY:',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+  )
+
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-  );
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  )
 }
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,5 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
 /**
  * Especially important if using Fluid compute: Don't put this client in a
@@ -7,7 +7,13 @@ import { cookies } from "next/headers";
  * it.
  */
 export async function createClient() {
-  const cookieStore = await cookies();
+  console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log(
+    'KEY:',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+  )
+  const cookieStore = await cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -15,20 +21,20 @@ export async function createClient() {
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll();
+          return cookieStore.getAll()
         },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            );
+              cookieStore.set(name, value, options)
+            )
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have proxy refreshing
             // user sessions.
           }
-        },
-      },
-    },
-  );
+        }
+      }
+    }
+  )
 }
