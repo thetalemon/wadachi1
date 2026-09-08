e8b3c1ec5a8369a2daf0ea9661eefbb8d5d31cd5
thetalemon <sasami0314@gmail.com>
2026-09-08 16:33:17 +0900

    不要になったHeroを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { EnvVarWarning } from '@/components/env-var-warning'
 import { AuthButton } from '@/components/auth-button'
-import { Hero } from '@/components/hero'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import { ConnectSupabaseSteps } from '@/components/tutorial/connect-supabase-steps'
 import { SignUpUserSteps } from '@/components/tutorial/sign-up-user-steps'
@@ -27,7 +26,6 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
