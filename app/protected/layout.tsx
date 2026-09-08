e30bf625b9eb80c423059ca9515c55518b4c0828
thetalemon <sasami0314@gmail.com>
2026-09-08 16:32:15 +0900

    不要になったデプロイボタンを削除

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,15 +1,14 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-import { Suspense } from "react";
+import { EnvVarWarning } from '@/components/env-var-warning'
+import { AuthButton } from '@/components/auth-button'
+import { ThemeSwitcher } from '@/components/theme-switcher'
+import { hasEnvVars } from '@/lib/utils'
+import Link from 'next/link'
+import { Suspense } from 'react'
 
 export default function ProtectedLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <main className="min-h-screen flex flex-col items-center">
@@ -17,10 +16,7 @@ export default function ProtectedLayout({
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
+              <Link href={'/'}>Next.js Supabase Starter</Link>
             </div>
             {!hasEnvVars ? (
               <EnvVarWarning />
@@ -37,7 +33,7 @@ export default function ProtectedLayout({
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
-            Powered by{" "}
+            Powered by{' '}
             <a
               href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
               target="_blank"
@@ -51,5 +47,5 @@ export default function ProtectedLayout({
         </footer>
       </div>
     </main>
-  );
+  )
 }
